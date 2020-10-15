6576f6f037ee9387495b13f56a3ee27872159c3e
Romanasboot <r.kuziakov@gmail.com>
2020-10-15 11:33:09 +0300

    initial nepilnas

---
+++ Skaitykla/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Skaitykla.Models;
+
+namespace Skaitykla.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.AuthError = "blogai ivesti duomenys";
+                return View("~/Views/Auth/Index.cshhtml");
+            }
+            return View();
+        }
+    }
+}
---
+++ Skaitykla/Models/LoginViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Skaitykla.Models
+{
+    public class LoginViewModel : Controller
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(10)]
+        public string Password { get; set; }
+    }
+}
